d27ebe8c82629df287d40c316cd12edaf817308c
andrewwang79 <andrewwang79@gmail.com>
2021-04-09 18:43:35 +0800

    kickoff

---
+++ autotest/src/main/java/cn/wangyaqi/java/practice/autotest/HelloController.java
@@ -0,0 +1,13 @@
+package cn.wangyaqi.java.practice.autotest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
