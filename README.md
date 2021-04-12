c5aaf6d3b9f1c7e41ecc93127a582ef74c70b428
andrewwang79 <andrewwang79@gmail.com>
2021-04-12 14:21:32 +0800

    no message

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 | autotest | 自动化测试：接口单元测试，接口集成测试 |
 
 ## misc
-| 功能 | 说明 |
-| Actuator | https://www.liaoxuefeng.com/wiki/1252599548343744/1282386381766689 |
+### Actuator
+* https://www.liaoxuefeng.com/wiki/1252599548343744/1282386381766689
 
+### Swagger
+* https://www.cnblogs.com/progor/p/13297904.html
+* http://localhost:8080/swagger-ui.html
---
+++ misc/src/main/java/cn/wangyaqi/java/practice/misc/MiscApplication.java
@@ -0,0 +1,13 @@
+package cn.wangyaqi.java.practice.misc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiscApplication.class, args);
+    }
+
+}
